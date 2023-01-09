2975bd0a5bb91656daf67ef88c87497f18e77c03
ibeandyy <adhyr4@gmail.com>
2023-01-09 12:57:16 -0800

    init commit

---
+++ src/Scraper.ts
@@ -0,0 +1,53 @@
+import puppeteer from "puppeteer";
+import { username, password } from "./config.json";
+import wait from "wait";
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://app.levelfields.ai");
+  await page.type(".Login_Input__XlFCw", username);
+  await page.click(".Login_PasswordWrapper__io09P");
+  await page.type(".Login_PasswordWrapper__io09P", password);
+  await page.click(".Login_Button__jkxUr[type=submit]");
+  await page.waitForNavigation();
+  await page.goto("https://app.levelfields.ai/dashboard");
+  // Scrape data from the dashboard page here
+  //   await page.click(".My Alerts");
+  //select the button named My Alerts
+  const button = await page.$x('//button[contains(text(), "My Alerts")]');
+  await button[0]?.click();
+  wait(5000);
+  const div = await page.$(".TableCell");
+  const companyNameNode = await page.waitForSelector(
+    "div.Table-row:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > div:nth-child(1)"
+  );
+  const scenarioNode = await page.waitForSelector(
+    "div.Table-row:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > p:nth-child(1)"
+  );
+  const bobNode = await page.waitForSelector(
+    "div.Table-row:nth-child(2) > div:nth-child(8) > div:nth-child(1) > p:nth-child(1) > img:nth-child(1)"
+  );
+  const dateNode = await page.waitForSelector(
+    "div.Table-row:nth-child(2) > div:nth-child(4) > div:nth-child(1) > p:nth-child(1)"
+  );
+  const date1 = await dateNode?.getProperty("textContent");
+  const companyName1 = await companyNameNode?.getProperty("textContent");
+  const scenario1 = await scenarioNode?.getProperty("textContent");
+  const bob1 = await bobNode?.getProperty("src");
+  //   /static/media/bear.c78a45665e1f859de9b4f1f0618c1a40.svg
+  const date = await date1?.jsonValue();
+  const companyName = await companyName1?.jsonValue();
+  const scenario = await scenario1?.jsonValue();
+  const bob = await bob1?.jsonValue();
+  console.log(companyName, scenario, bob);
+  //   await page.waitForNavigation();
+  return {
+    ticker: companyName,
+    scenario: scenario,
+    bob: bob?.endsWith("bear.c78a45665e1f859de9b4f1f0618c1a40.svg")
+      ? "bear"
+      : "bull",
+    date: date,
+  };
+};
+main();
